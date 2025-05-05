f3f97e6c7cd495ff4fa08c6235741b9b0d6b3a81
Carlos Aguilera <18aguilerac@gmail.com>
2025-05-05 11:47:57 -0700

    added theme and zod

---
+++ components/AppForm.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import * as z from "zod";
+import type { FormSubmitEvent } from "@nuxt/ui";
+
+defineProps<{
+  color:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "info"
+    | "warning"
+    | "error"
+    | "neutral";
+}>();
+
+const schema = z.object({
+  email: z.string().email("Invalid email"),
+  password: z.string().min(8, "Must be at least 8 characters"),
+});
+
+type Schema = z.output<typeof schema>;
+
+const state = reactive<Partial<Schema>>({
+  email: undefined,
+  password: undefined,
+});
+
+const toast = useToast();
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  toast.add({
+    title: "Success",
+    description: "The form has been submitted.",
+    color: "success",
+  });
+  console.log(event.data);
+}
+</script>
+
+<template>
+  <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+    <UFormField label="Email" name="email">
+      <UInput v-model="state.email" :color="color" />
+    </UFormField>
+
+    <UFormField label="Password" name="password">
+      <UInput v-model="state.password" type="password" :color="color" />
+    </UFormField>
+
+    <UButton type="submit" :color="color"> Submit </UButton>
+  </UForm>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import type { BreadcrumbItem } from "@nuxt/ui";
+
+const page = ref(5);
+const items1 = ref([
+  "primary",
+  "secondary",
+  "success",
+  "info",
+  "warning",
+  "error",
+  "neutral",
+]);
+
+const items2 = ref<BreadcrumbItem[]>([
+  {
+    label: "Home",
+    icon: "i-lucide-house",
+  },
+  {
+    label: "Components",
+    icon: "i-lucide-box",
+    to: "/components",
+  },
+  {
+    label: "Breadcrumb",
+    icon: "i-lucide-link",
+    to: "/components/breadcrumb",
+  },
+]);
+
+const color = ref<
+  "primary" | "secondary" | "success" | "info" | "warning" | "error" | "neutral"
+>("primary");
+</script>
+
+<template>
+  <div class="p-10">
+    <ThemeButton />
+    <AppAlert>This is an auto-imported component</AppAlert>
+    <div
+      class="grid grid-cols-4 gap-4 justify-center items-center justify-items-center p-8 border mb-5"
+    >
+      <UAlert
+        title="Heads up!"
+        description="You can change the primary color in your app config."
+        icon="i-lucide-terminal"
+        :color="color"
+      />
+      <UButton :color="color">Button</UButton>
+      <UCalendar :color="color" />
+      <UCheckbox :color="color" />
+      <AppForm :color="color" />
+      <UPagination v-model:page="page" :total="100" :color="color" />
+      <USelect v-model="color" :items="items1" class="w-48" :color="color" />
+      <USwitch :color="color" />
+      <UBreadcrumb :items="items2" :color="color" />
+    </div>
+    <AppTable :color="color" />
+  </div>
+</template>
