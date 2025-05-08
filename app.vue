65f2a405b6d8083ec984f8e1573602b8d31e9e4c
Carlos Aguilera <18aguilerac@gmail.com>
2025-05-08 01:18:25 -0700

    add favicon and seo

--- app.vue
+++ app.vue
@@ -1,3 +1,19 @@
+<script lang="ts">
+if (import.meta.server) {
+  useSeoMeta({
+    title: "Devlos Shop",
+    ogTitle: "Devlos Shop",
+    description: "Shop unique and interesting tech at Devlos Shop",
+    ogDescription: "Shop unique and interesting tech at Devlos Shop",
+    ogImage: "/logo.png",
+    ogType: "website",
+    twitterCard: "summary_large_image",
+    twitterTitle: "Devlos Shop",
+    twitterDescription: "Shop unique and interesting tech at Devlos Shop",
+  });
+}
+</script>
+
 <template>
   <UApp>
     <NuxtPage />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Devlos Shop",
+      htmlAttrs: {
+        lang: "en",
+      },
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
   modules: ["@nuxt/ui"],
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
