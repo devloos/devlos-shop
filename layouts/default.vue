5f8e2e508b50ea0eb646491526b2932e5262011c
Carlos Aguilera <18aguilerac@gmail.com>
2025-05-16 10:19:09 -0700

    refactor: update search and menu toggle functions for improved functionality and clarity feat: add hamburger SVG icon for navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,18 +4,35 @@ const user = useSupabaseUser();
 const isSearching = ref(false);
 const search = ref('');
 
-function closeSearch() {
+function toggleSearch() {
   isSearching.value = !isSearching.value;
   search.value = '';
+
+  menuOpen.value = false;
+}
+
+const menuOpen = ref(false);
+
+function toggleMenu() {
+  menuOpen.value = !menuOpen.value;
+
+  isSearching.value = false;
+  search.value = '';
+}
+
+function closeNav() {
+  isSearching.value = false;
+  search.value = '';
+  menuOpen.value = false;
 }
 </script>
 
 <template>
   <div>
     <header
-      class="nav border-content-300/25 dark:border-content-300/50 border bg-linear-to-t from-dark/5 dark:from-dark/10 from-25% dark:from-50% to-light/30 dark:to-light/5 fixed top-0 left-0 right-0 max-w-8/12 mx-auto z-10 my-4 backdrop-blur-sm py-4 px-6 rounded-lg"
+      class="nav border-content-300/25 dark:border-content-300/50 border bg-linear-to-t from-dark/5 dark:from-dark/10 from-25% dark:from-50% to-light/30 dark:to-light/5 fixed top-0 left-0 right-0 lg:max-w-8/12 md:max-w-10/12 max-w-11/12 mx-auto z-10 my-4 backdrop-blur-sm py-4 px-4 md:px-6 rounded-lg"
       :class="{
-        'nav-open': isSearching && !!search,
+        'nav-open': (isSearching && !!search) || menuOpen,
       }"
     >
       <div class="flex justify-between items-center">
@@ -25,11 +42,16 @@ function closeSearch() {
             class="inline-flex justify-start items-center gap-3 hover:scale-[1.01] transition-all"
           >
             <Icon class="text-4xl" name="svgs:logo" />
-            <p class="font-semibold text-lg">Devlos Shop</p>
+            <p v-if="!isSearching" class="md:hidden xl:block font-semibold text-lg">
+              Devlos Shop
+            </p>
           </NuxtLink>
         </div>
 
-        <div v-if="!isSearching" class="flex grow justify-center gap-10">
+        <div
+          v-if="!isSearching"
+          class="hidden md:flex grow justify-center md:gap-5 xl:gap-10"
+        >
           <NuxtLink to="privacy" class="hover:text-primary/50 transition-all">
             Shop
           </NuxtLink>
@@ -47,11 +69,11 @@ function closeSearch() {
           </NuxtLink>
         </div>
 
-        <div class="flex grow justify-end gap-1 items-center transition-all">
+        <div class="flex grow justify-end lg:gap-1 items-center transition-all">
           <UButtonGroup
             class="transition-all duration-300 ease-in-out"
             :class="{
-              'grow px-5': isSearching,
+              'grow ps-3 pe-1 md:ps-5': isSearching,
             }"
           >
             <UButton
@@ -59,7 +81,7 @@ function closeSearch() {
               color="neutral"
               :variant="isSearching ? 'outline' : 'ghost'"
               size="xl"
-              @click="closeSearch"
+              @click="toggleSearch"
             />
             <template v-if="isSearching">
               <UInput
@@ -84,24 +106,32 @@ function closeSearch() {
 
           <template v-if="user">
             <UButton
+              class="hidden md:flex"
               icon="svgs:profile"
               color="neutral"
               variant="ghost"
               size="xl"
               @click="navigateTo('/account')"
             />
-            <UButton icon="svgs:shopping-bag" color="neutral" variant="ghost" size="xl" />
+            <UButton
+              class="hidden md:flex"
+              icon="svgs:shopping-bag"
+              color="neutral"
+              variant="ghost"
+              size="xl"
+            />
           </template>
           <template v-else>
             <div class="flex gap-4 items-center justify-end">
               <UButton
+                class="hidden md:flex"
                 icon="svgs:shopping-bag"
                 color="neutral"
                 variant="ghost"
                 size="xl"
               />
 
-              <div class="relative inline-flex group">
+              <div class="hidden md:inline-flex relative group">
                 <div
                   class="absolute opacity-90 inset-2.5 bg-primary rounded-2xl blur-lg group-hover:opacity-100 group-hover:inset-2 group-hover:duration-300 transition-all"
                 />
@@ -114,8 +144,18 @@ function closeSearch() {
               </div>
             </div>
           </template>
+
+          <UButton
+            class="md:hidden"
+            :icon="menuOpen ? 'svgs:x' : 'svgs:hamburger'"
+            color="neutral"
+            variant="ghost"
+            size="xl"
+            @click="toggleMenu"
+          />
         </div>
       </div>
+
       <div class="p-3">
         <p>SEARCHING</p>
         <p>SEARCHING</p>
@@ -123,8 +163,12 @@ function closeSearch() {
         <p>SEARCHING</p>
       </div>
     </header>
-    <slot />
-    footer
+
+    <div @click="closeNav">
+      <slot />
+    </div>
+
+    <footer>FOOTER</footer>
   </div>
 </template>
 
