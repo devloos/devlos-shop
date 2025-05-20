71b2c9a40b5048cf9107c66fde2f13ef35ae0662
Carlos Aguilera <18aguilerac@gmail.com>
2025-05-20 16:23:33 -0700

    feat: use three js and add grid

---
+++ assets/constants/tile-grid.ts
@@ -0,0 +1,246 @@
+import { Vector3 } from 'three';
+
+export const TILE_GRID = [
+  [
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(0, 0, 0),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(1.05, 0, 0),
+    },
+    {
+      path: '/models/slab/slab-accent.glb',
+      position: new Vector3(2.1, 0, 0),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(3.15, 0, 0),
+    },
+    {
+      path: '/models/piller/piller-accent.glb',
+      position: new Vector3(4.2, 0, 0),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-1.05, 0, 0),
+    },
+    {
+      path: '/models/slab/slab-gray.glb',
+      position: new Vector3(-2.1, 0, 0),
+    },
+    {
+      path: '/models/slab/slab-gray.glb',
+      position: new Vector3(-3.15, 0, 0),
+    },
+    {
+      path: '/models/piller/piller-secondary.glb',
+      position: new Vector3(-4.2, 0, 0),
+    },
+  ],
+  [
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(1.05, 0, 1.05),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(2.1, 0, 1.05),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-1.05, 0, 1.05),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-2.1, 0, 1.05),
+    },
+  ],
+  [
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(1.05, 0, 2.1),
+    },
+    {
+      path: '/models/slab/slab-gray.glb',
+      position: new Vector3(2.1, 0, 2.1),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-1.05, 0, 2.1),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-2.1, 0, 2.1),
+    },
+    {
+      path: '/models/slab/slab-accent.glb',
+      position: new Vector3(-3.15, 0, 2.1),
+    },
+    {
+      path: '/models/slab/slab-accent.glb',
+      position: new Vector3(-4.2, 0, 2.1),
+    },
+    {
+      path: '/models/piller/piller-secondary.glb',
+      position: new Vector3(-5.25, 0, 2.1),
+    },
+  ],
+  [
+    {
+      path: '/models/slab/slab-gray.glb',
+      position: new Vector3(1.05, 0, 3.15),
+    },
+    {
+      path: '/models/slab/slab-gray.glb',
+      position: new Vector3(2.1, 0, 3.15),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(3.15, 0, 3.15),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(4.2, 0, 3.15),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-2.1, 0, 3.15),
+    },
+  ],
+  [
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(1.05, 0, 4.2),
+    },
+    {
+      path: '/models/slab/slab-accent.glb',
+      position: new Vector3(2.1, 0, 4.2),
+    },
+    {
+      path: '/models/slab/slab-gray.glb',
+      position: new Vector3(3.15, 0, 4.2),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(4.2, 0, 4.2),
+    },
+    {
+      path: '/models/piller/piller-secondary.glb',
+      position: new Vector3(-2.1, 0, 4.2),
+    },
+  ],
+  [
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(0, 0, -1.05),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(1.05, 0, -1.05),
+    },
+    {
+      path: '/models/slab/slab-accent.glb',
+      position: new Vector3(4.2, 0, -1.05),
+    },
+    {
+      path: '/models/slab/slab-accent.glb',
+      position: new Vector3(-1.05, 0, -1.05),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-2.1, 0, -1.05),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-3.15, 0, -1.05),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-4.2, 0, -1.05),
+    },
+  ],
+  [
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(0, 0, -2.1),
+    },
+    {
+      path: '/models/slab/slab-gray.glb',
+      position: new Vector3(1.05, 0, -2.1),
+    },
+    {
+      path: '/models/slab/slab-gray.glb',
+      position: new Vector3(4.2, 0, -2.1),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-3.15, 0, -2.1),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-4.2, 0, -2.1),
+    },
+  ],
+  [
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(0, 0, -3.15),
+    },
+    {
+      path: '/models/slab/slab-accent.glb',
+      position: new Vector3(1.05, 0, -3.15),
+    },
+    {
+      path: '/models/slab/slab-gray.glb',
+      position: new Vector3(4.2, 0, -3.15),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-3.15, 0, -3.15),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-4.2, 0, -3.15),
+    },
+  ],
+  [
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(0, 0, -4.2),
+    },
+    {
+      path: '/models/slab/slab-accent.glb',
+      position: new Vector3(1.05, 0, -4.2),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(2.1, 0, -4.2),
+    },
+    {
+      path: '/models/piller/piller-secondary.glb',
+      position: new Vector3(3.15, 0, -4.2),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-1.05, 0, -4.2),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-2.1, 0, -4.2),
+    },
+    {
+      path: '/models/slab/slab-accent.glb',
+      position: new Vector3(-3.15, 0, -4.2),
+    },
+    {
+      path: '/models/slab/slab-secondary.glb',
+      position: new Vector3(-4.2, 0, -4.2),
+    },
+    {
+      path: '/models/piller/piller-gray.glb',
+      position: new Vector3(-5.25, 0, -4.2),
+    },
+  ],
+];
--- components/HeroModel.vue
+++ components/HeroModel.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { MouseParallax } from '@tresjs/cientos';
+import { MouseParallax, GLTFModel } from '@tresjs/cientos';
+import { TILE_GRID } from '~/assets/constants/tile-grid';
 
 const LIGHT_BG_COLOR = '#F1F2F4';
 const DARK_BG_COLOR = '#0B0B0B';
@@ -11,12 +12,18 @@ const clearColor = computed(() => (isDark.value ? DARK_BG_COLOR : LIGHT_BG_COLOR
 
 <template>
   <TresCanvas :clear-color="clearColor">
-    <TresPerspectiveCamera :position="[3, 3, 3]" :look-at="[0, 0, 0]" />
+    <TresPerspectiveCamera :position="[4, 3, 4]" :look-at="[0, 0, 0]" />
     <MouseParallax :factor="-0.7" :ease="2" local />
-    <TresMesh>
-      <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
-      <TresMeshBasicMaterial color="orange" />
-    </TresMesh>
+
+    <template v-for="(row, i) in TILE_GRID" :key="i">
+      <Suspense v-for="(tile, index) in row" :key="index">
+        <TresMesh :position="tile.position">
+          <GLTFModel :path="tile.path" />
+        </TresMesh>
+      </Suspense>
+    </template>
+
+    <TresDirectionalLight :intensity="4" :position="[-3, 1, 3]" />
     <TresAmbientLight :intensity="1" />
   </TresCanvas>
 </template>
