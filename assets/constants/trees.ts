00de6b12254318b5f4c01e2f8ec9568077505bc8
Carlos Aguilera <18aguilerac@gmail.com>
2025-05-20 17:21:48 -0700

    feat: add trees

---
+++ assets/constants/trees.ts
@@ -0,0 +1,24 @@
+import { Vector3 } from 'three';
+
+export const TREES = [
+  {
+    path: '/models/tree/tree.glb',
+    position: new Vector3(-3.15, 0, 2.1),
+  },
+  {
+    path: '/models/tree/tree.glb',
+    position: new Vector3(-4.2, 0, -1.05),
+  },
+  {
+    path: '/models/tree/tree.glb',
+    position: new Vector3(0, 0, -3.15),
+  },
+  {
+    path: '/models/tree/tree.glb',
+    position: new Vector3(1.05, 0, 4.2),
+  },
+  {
+    path: '/models/tree/tree.glb',
+    position: new Vector3(3.15, 0, -4.2),
+  },
+];
--- components/HeroModel.vue
+++ components/HeroModel.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { MouseParallax, GLTFModel } from '@tresjs/cientos';
+import { MouseParallax, GLTFModel, OrbitControls } from '@tresjs/cientos';
 import { TILE_GRID } from '~/assets/constants/tile-grid';
+import { TREES } from '~/assets/constants/trees';
 
 const LIGHT_BG_COLOR = '#F1F2F4';
 const DARK_BG_COLOR = '#0B0B0B';
@@ -11,9 +12,10 @@ const clearColor = computed(() => (isDark.value ? DARK_BG_COLOR : LIGHT_BG_COLOR
 </script>
 
 <template>
-  <TresCanvas :clear-color="clearColor">
+  <TresCanvas :clear-color="clearColor" shadows>
     <TresPerspectiveCamera :position="[4, 3, 4]" :look-at="[0, 0, 0]" />
     <MouseParallax :factor="-0.7" :ease="2" local />
+    <OrbitControls v-if="false" />
 
     <template v-for="(row, i) in TILE_GRID" :key="i">
       <Suspense v-for="(tile, index) in row" :key="index">
@@ -23,7 +25,13 @@ const clearColor = computed(() => (isDark.value ? DARK_BG_COLOR : LIGHT_BG_COLOR
       </Suspense>
     </template>
 
-    <TresDirectionalLight :intensity="4" :position="[-3, 1, 3]" />
+    <Suspense v-for="(tree, index) in TREES" :key="index">
+      <TresMesh :position="tree.position">
+        <GLTFModel :path="tree.path" />
+      </TresMesh>
+    </Suspense>
+
+    <TresDirectionalLight :intensity="4" :position="[-3, 1, 3]" cast-shadow />
     <TresAmbientLight :intensity="1" />
   </TresCanvas>
 </template>
